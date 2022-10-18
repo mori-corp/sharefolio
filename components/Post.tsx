fc18249c249089b37d25fe622973f1f9ecc9665b
Shintaro <mori@hey.com>
2022-10-18 12:09:45 +0900

    Fix: Lorem text

--- components/Post.tsx
+++ components/Post.tsx
@@ -131,7 +131,7 @@ export default function ArticleList() {
             fontSize="md"
           >
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
+            industry. Lorem Ipsum has been the industrys standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book.
           </Text>
