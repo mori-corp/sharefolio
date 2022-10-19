d6ee54c459a9039e94c3aaeed587b84a492b9a7e
Shintaro <mori@hey.com>
2022-10-19 11:54:54 +0900

    投稿者情報をコンポーネント化

---
+++ components/Author.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Image, Text, HStack, Icon } from "@chakra-ui/react";
+import { AiOutlineHeart } from "react-icons/ai";
+
+// 投稿者のデータ型定義
+type PosterProps = {
+  date: Date | null;
+  name: string;
+};
+
+export const Author: React.FC<PosterProps> = (props) => {
+  return (
+    <HStack spacing="2" display="flex" alignItems="center">
+      {/* プロフィール画像 */}
+      <Image
+        borderRadius="full"
+        boxSize="36px"
+        src="https://100k-faces.glitch.me/random-image"
+        alt={`Avatar of ${props.name}`}
+      />
+
+      {/* ユーザーネーム */}
+      <Text fontWeight="medium">{props.name}</Text>
+      <Text>—</Text>
+
+      {/* 投稿日時 */}
+      <Text>{props.date?.toLocaleDateString()}</Text>
+    </HStack>
+  );
+};
