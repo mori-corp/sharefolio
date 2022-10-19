f2c52e157c4be6b691f3ec1bc56d33bf1761ab31
Shintaro <mori@hey.com>
2022-10-19 11:57:09 +0900

    実際のUIに近づけるため、投稿内容を複数用意。

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import type { NextPage } from "next";
 import Layout from "../../components/Layout";
 import Post from "../../components/Post";
+import { Heading } from "@chakra-ui/react";
 
 const index: NextPage = () => {
   return (
     <Layout title={"Posts - ShareFolio"}>
+      <Heading as="h1" textAlign={"center"} mt={16}>
+        投稿一覧
+      </Heading>
+      <Post />
+      <Post />
+      <Post />
       <Post />
     </Layout>
   );
