1c38825044e18e49cbfed8f625ded72568bd1611
Shintaro <mori@hey.com>
2022-10-26 12:01:19 +0900

    各投稿のデータ型を変更

--- components/LanguageTags.tsx
+++ components/LanguageTags.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Tag, SpaceProps, Wrap } from "@chakra-ui/react";
 
 type ILanguageTags = {
-  tags: Array<string>;
+  tags: Array<string> | undefined;
   marginTop?: SpaceProps["marginTop"];
 };
 
 export const LanguageTags: React.FC<ILanguageTags> = (props) => {
   return (
     <Wrap spacing={1} marginTop={2}>
-      {props.tags.map((tag) => {
+      {props.tags?.map((tag) => {
         return (
           <Tag size={"sm"} variant="solid" colorScheme="pink" key={tag}>
             {tag}
