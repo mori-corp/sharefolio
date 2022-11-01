9dc2dffcf714dbfb6221577d62c4f261ff160ebc
Shintaro <mori@hey.com>
2022-11-01 12:03:48 +0900

    githubアイコンのリンクを新規タブで開くように設定

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,7 @@ import {
   Stack,
   Text,
   Image,
+  Link,
 } from "@chakra-ui/react";
 import * as React from "react";
 import { FaGithub, FaTwitter } from "react-icons/fa";
@@ -15,17 +16,17 @@ export const Footer = () => (
       <Stack justify="start" direction="row" align="center">
         <Image src="/logo.png" alt="logo" w={"100px"} />
         <ButtonGroup variant="ghost">
-          <IconButton
-            as="a"
-            href="https://github.com/mori-corp/sharefolio"
-            aria-label="GitHub"
-            icon={<FaGithub fontSize="1.25rem" />}
-          />
-          <IconButton
+          <Link href="https://github.com/mori-corp/sharefolio" isExternal>
+            <IconButton
+              aria-label="GitHub"
+              icon={<FaGithub fontSize="1.25rem" />}
+            />
+          </Link>
+          {/* <IconButton
             as="a"
             aria-label="Twitter"
             icon={<FaTwitter fontSize="1.25rem" />}
-          />
+          /> */}
         </ButtonGroup>
       </Stack>
       <Text fontSize="sm" color="subtle">
