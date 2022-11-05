e047ac1ce952bdf94af6bdb24d2a4f5097e5b4fe
Shintaro <mori@hey.com>
2022-11-05 10:33:01 +0900

    ログイン済みの場合は、認証ページへのアクセス制限

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
 import { AuthPage } from "../components/AuthPage";
 import Layout from "../components/Layout";
+import { useAuth } from "../lib/auth";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const user = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user.uid) {
+      router.push("/posts");
+    } else {
+      return;
+    }
+  }, []);
   return (
     <Layout title={"ShareFolio｜ポートフォリオの共有サイト"}>
       <AuthPage />
