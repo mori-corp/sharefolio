5f1d76a8a3d18d88e50dfc113c004d53b0070b23
Shintaro <mori@hey.com>
2022-11-08 11:27:14 +0900

    pathの修正

--- components/navigationBar/DesktopNav.tsx
+++ components/navigationBar/DesktopNav.tsx
@@ -1,4 +1,4 @@
-import { auth } from "firebase";
+import { auth } from "../../firebase";
 import { signOut } from "firebase/auth";
 import { useRecoilState } from "recoil";
 import { userState } from "@/lib/auth";
